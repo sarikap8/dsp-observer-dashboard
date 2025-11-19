433a5379c3391e1ab4093e29a8779534fc0b5079
isa3l <isaellarios936@gmail.com>
2025-11-18 21:02:20 -0800

    lol

--- my-app/src/app/VolunteerDashboard.tsx
+++ my-app/src/app/VolunteerDashboard.tsx
@@ -135,24 +135,7 @@ const VolunteerDashboard = () => {
               />
             </div>
 
-            {/* Department Filter */}
-            <div className="mb-6">
-              <div className="relative">
-                <Filter className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-gray-400" />
-                <select
-                  value={departmentFilter}
-                  onChange={(e) => setDepartmentFilter(e.target.value)}
-                  className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg appearance-none cursor-pointer focus:ring-2 focus:ring-purple-500 focus:border-transparent"
-                >
-                  <option value="all">All Departments</option>
-                  {departments.slice(1).map((dept) => (
-                    <option key={dept} value={dept}>
-                      {dept}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            </div>
+            
 
             {/* Volunteer Cards */}
             <div className="space-y-3">
