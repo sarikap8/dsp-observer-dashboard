d1d565d64e12aeb4dc9aed44905fe947779150f9
isa3l <isaellarios936@gmail.com>
2025-12-12 21:59:36 -0800

    done

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,21 +1,23 @@
-<<<<<<< HEAD
 'use client';
 
+import { useState } from "react";
 import VolunteerDashboard from "../comparison/VolunteerDashboard";
-=======
 import Login from "./login/login";
-import {GoogleOAuthProvider, GoogleLogin} from '@react-oauth/google';
->>>>>>> 78e3a58c51315db33af17614302d34bbb1931eb9
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const clientId = '221765598159-7nn7s0ek0q8n34iojmgijh4gndnfrtkf.apps.googleusercontent.com';
+const clientId =
+  "221765598159-7nn7s0ek0q8n34iojmgijh4gndnfrtkf.apps.googleusercontent.com";
+
 export default function Home() {
-<<<<<<< HEAD
-  return <VolunteerDashboard />;
-=======
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <GoogleOAuthProvider clientId={clientId}>
-      <Login />
+      {isLoggedIn ? (
+        <VolunteerDashboard />
+      ) : (
+        <Login onSuccess={() => setIsLoggedIn(true)} />
+      )}
     </GoogleOAuthProvider>
   );
->>>>>>> 78e3a58c51315db33af17614302d34bbb1931eb9
 }
